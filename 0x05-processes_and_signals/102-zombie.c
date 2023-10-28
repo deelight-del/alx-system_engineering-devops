5bc79eace5e450a3481565f9e1ff78f99e8e13d5
deelight-del <akingbenidavidoyin@gmail.com>
2023-10-28 04:12:44 +0000

    refix task 102

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -3,6 +3,21 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+
+/**
+ * infinite_while - function loop through.
+ *
+ * Return: An integer. 0.
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
 /**
  * main - Function to create zombie processes
  *
@@ -18,10 +33,15 @@ int main(void)
 	for (i = 0; i < 5; i++)
 	{
 		pid = fork();
+		if (pid == 0)
+		{
+			exit(0);
+		}
 		if (pid > 0)
 		{
 			printf("Zombie process created, PID: %u\n", pid);
 		}
 	}
+	infinite_while();
 	return (0);
 }
