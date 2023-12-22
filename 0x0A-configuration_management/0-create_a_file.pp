5c1ec01fd159a7f2ad1c36203746ddd1646ec389
deelight-del <akingbenidavidoyin@gmail.com>
2023-12-22 22:23:29 +0000

    added comma to fix syntax issue

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
 # Puppet file to create a file temp
 
 file {'/tmp/school':
-  ensure  => file
-  path    => '/tmp/school'
-  mode    => '0744'
-  owner   => 'www-data'
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
   content =>  'I love Puppet'
+
 }
