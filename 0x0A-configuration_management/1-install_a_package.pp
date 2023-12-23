0bfda1f968dfacd59c1f620bc9a2841ab546ace6
deelight-del <akingbenidavidoyin@gmail.com>
2023-12-23 04:25:34 +0000

    Task 1

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,25 @@
+# Installing a flask package
+
+#Using the exec command to ensure that we apt-update
+
+#exec {'apt-update':
+#  command => '/usr/bin/apt-get update'
+#}
+
+#Install python 3.8.10
+package {'python3.8':
+  ensure =>  present,
+}
+
+#Install pip and ensrue that the system is update before doing it
+package {'python3-pip3':
+  ensure  => present,
+}
+
+# Install flask and enure this pip is installed first.
+
+package {'flask':
+  ensure   => '2.1.0',
+  require  => Package['python3-pip3'],
+  provider => 'pip',
+}
