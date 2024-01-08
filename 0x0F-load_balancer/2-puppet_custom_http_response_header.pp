c9a980ec8ccba219b53bf74499ac16988f17b8db
deelight-del <akingbenidavidoyin@gmail.com>
2024-01-08 18:27:15 +0000

    Task 2, using service instead of exec

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -7,15 +7,15 @@ package {'nginx':
 }
 
 #Replacing content in the lines of the config files
-file_line {'default':
+file_line {'replace a default':
   path     => '/etc/nginx/sites-available/default',
   line     => "\t\tadd_header X-Served-By \"${facts['networking']['hostname']}\";",
   after    => '^\s*location / {',
   multiple => false,
 }
 
-#Executing nginx
-exec {'nginx':
-  command => '/etc/init.d/nginx restart',
+#Running nginx service
+service {'nginx':
+  ensure  => running,
   require => Package['nginx']
 }
