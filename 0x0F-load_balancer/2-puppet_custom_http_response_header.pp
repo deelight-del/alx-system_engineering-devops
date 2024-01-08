2d1b1e5a0ea965aa15e15c15561d4d33315bb77c
deelight-del <akingbenidavidoyin@gmail.com>
2024-01-08 18:08:31 +0000

    Add installation of haproxy

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,21 @@
 #Puppeteering and automating adding HTTP header to our respective web servers
 include stdlib
 
+## Installing haproxy
+package {'haproxy':
+  ensure => 'installed'
+}
+
+#Replacing content in the lines of the config files
 file_line {'default':
   path     => '/etc/nginx/sites-available/default',
-  line     => "\t\tadd_header X-Served-By \"${facts['networking']['hostname']}\"",
+  line     => "\t\tadd_header X-Served-By \"${facts['networking']['hostname']}\";",
   after    => '^\s*location / {',
   multiple => false,
+}
+
+#Executing haproxy
+exec {'haproxy':
+  command => '/etc/init.d/haproxy restart',
+  require => Package['haproxy']
 }
