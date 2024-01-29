10f1c6d71f5f0b3ff68d9f45649fff1f8ee1e962
deelight-del <akingbenidavidoyin@gmail.com>
2024-01-29 14:40:11 +0000

    Task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" This file accesses a REST API
+and prints out some information based on the passed id
+as an integer"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    emp_id = int(sys.argv[1])
+    if isinstance(emp_id, int):
+        users_url = 'https://jsonplaceholder.typicode.com/users'
+        todos_url = 'https://jsonplaceholder.typicode.com/todos'
+        users_list_json = requests.get(users_url)
+        users_list = users_list_json.json()
+        for user in users_list:
+            if user.get('id') == emp_id:
+                emp_name = user.get('name')
+                todos_list_json = requests.get(todos_url)
+                todos_list = todos_list_json.json()
+                todo_title_completed = {
+                    todo.get('title'): todo.get('completed') for todo in
+                    todos_list if todo.get('userId') == emp_id
+                }
+                total = len(todo_title_completed)
+                done = sum(todo_title_completed.values())
+                print(
+                    f"Employee {emp_name} is done with tasks({done}/{total})"
+                )
+                for title, completed in todo_title_completed.items():
+                    if completed:
+                        print(f"\t {title}")
+                break
---
+++ 0x15-api/README.md
@@ -0,0 +1,7 @@
+# Using API.
+
+API and API calls is fundamental to the instrmentation
+of microservices as software system solutions.
+
+This directory contains access to a free API and manipulation and retrieval
+of some content.
