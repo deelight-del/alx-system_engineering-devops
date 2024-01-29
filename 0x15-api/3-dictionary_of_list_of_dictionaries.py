6ad34c0d81c66b1c5811eb34e197c17cafb56494
deelight-del <akingbenidavidoyin@gmail.com>
2024-01-29 17:44:48 +0000

    Task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" This file accesses a REST API
+and prints out some information based on the passed id
+as an integer"""
+
+
+if __name__ == "__main__":
+    import json
+    import requests
+    import sys
+
+    users_url = 'https://jsonplaceholder.typicode.com/users'
+    todos_url = 'https://jsonplaceholder.typicode.com/todos'
+    emp_list = requests.get(users_url).json()
+    todo_all_dict = dict()
+    for emp in emp_list:
+        emp_id = emp.get('id')
+        each_id_list = []
+        todos_list = requests.get(todos_url).json()
+        for todo in todos_list:
+            if todo.get('userId') == emp.get('id'):
+                each_id_list.append(
+                        {
+                            'username': emp.get('username'),
+                            'task': todo.get('title'),
+                            'completed': todo.get('completed')
+                            }
+                        )
+        emp_id_dict = {emp.get('id'): each_id_list}
+        todo_all_dict.update(emp_id_dict)
+    with open('todo_all_employess.json', 'w') as outfile:
+        json.dump(todo_all_dict, outfile)
