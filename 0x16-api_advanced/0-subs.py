0abc98f1f88fbfad343fde803b37fed219bdf746
deelight-del <akingbenidavidoyin@gmail.com>
2024-02-13 11:20:46 +0000

    task fix, task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Querying for the amount of users
 in a subredding"""
+
+
 import requests
 
 
@@ -16,9 +18,15 @@ def number_of_subscribers(subreddit):
     """
     if subreddit is None or not isinstance(subreddit, str):
         return 0
+    headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+            AppleWebKit/537.36 (KHTML, like Gecko)\
+            Chrome/102.0.0.0 Safari/537.36'
+            }
     r = requests.get(
             f"https://www.reddit.com/r/{subreddit}/about.json",
-            allow_redirects=False
+            allow_redirects=False,
+            headers=headers
             )
     if r.status_code != 200:
         return 0
