6560138ac1d562e4b180072e707efe5fb3a9d9dd
deelight-del <akingbenidavidoyin@gmail.com>
2024-02-22 10:29:36 +0000

    task 0 and task 1 ammend

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,16 +1,13 @@
-# Using puppet to raise user file limits.
-
+#Puppet resource to increase file open limit
 
 exec {'sed replace':
-  command  =>   [
-    'sed -i "s/^holberton hard.*/holberton hard nofile 2048/" /etc/security/limits.conf',
-  ],
-  path     =>   '/bin',
-  provider =>    'shell',
+  command =>  'sed -i "s/^holberton hard.*/holberton hard nofile 2048/" /etc/security/limits.conf',
+  path =>  '/bin',
+  provider =>   'shell',
+}
+exec {'sed replacement':
+  command => 'sed -i "s/^holberton soft.*/holberton soft nofile 1024/" /etc/security/limits.conf',
+  path =>  '/bin',
+  provider =>   'shell',
 }
 
-exec {'sed replacement':
-  command  =>   'sed -i "s/^holberton soft.*/holberton soft nofile 1024/" /etc/security/limits.conf',
-  path     =>   '/bin',
-  provider =>    'shell',
-}
